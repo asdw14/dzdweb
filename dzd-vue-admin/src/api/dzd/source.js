fe422517f0da305ade0a28f23bf839eea4872d4d
dizhongdi <2755063993@qq.com>
2022-08-22 23:57:33 +0800

    资源后台管理

---
+++ dzd-vue-admin/src/api/dzd/source.js
@@ -0,0 +1,59 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/oss/source'
+
+export default {
+    //上传文件
+    upload(sourceInfo){
+        return request({
+            url : `${api_name}/upload`,
+            method: 'post',
+            data : sourceInfo
+        })
+
+    },
+
+  //主页分页获取公开资源
+  getPublicPageList(page,limit,searchObj) {
+    return request({
+      url: `${api_name}/getPublicPageList/${page}/${limit}`,
+      method: 'post',
+      data : searchObj
+    })
+  },
+    // 获取文章基本信息
+    getSourceInfoById(id){
+        return request({
+            url : `${api_name}/sourceInfo/${id}`,
+            method: 'get',
+        })
+
+    },
+
+    //修改资源信息，如名称，下载量之类
+    updateSourceInfo(id,sourceInfo){
+        return request({
+            url : `${api_name}/update/${id}`,
+            method: 'put',
+            data : sourceInfo
+        })
+
+    },
+
+    //根据id修改封禁状态，封禁改为未封禁，未封禁改为封禁                 
+    banById(id){
+        return request({
+            url: `${api_name}/banById/${id}`,      
+            method: 'put'
+          })
+      },
+
+    //删除资源by Id
+    removeById(id){
+        return request({
+            url: `${api_name}/removeById/${id}`,      
+            method: 'delete'
+          })
+      }
+
+}
---
+++ dzd-vue-admin/src/views/dzd/source/list.vue
@@ -0,0 +1,227 @@
+<template>
+  <div class="app-container">
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <!-- 标题 -->
+      <el-form-item size="mini">
+        <el-input v-model="searchObj.sourceName" placeholder="资源名称" />
+      </el-form-item>
+      <template size="mini">
+        <el-radio v-model="searchObj.isCharge" label="true" size="mini"
+          >免费</el-radio
+        >
+        <el-radio v-model="searchObj.isCharge" label="false" size="mini"
+          >收费</el-radio
+        >
+      </template>
+      <el-button
+        type="primary"
+        icon="el-icon-search"
+        @click="getPublicPageList()"
+        size="mini"
+        >查询</el-button
+      >
+      <el-button type="default" @click="resetData()" size="mini"
+        >清空</el-button
+      >
+    </el-form>
+
+    <!-- 表格 -->
+    <el-table
+      v-loading="listLoading"
+      :data="sourceList"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row
+      row-class-name="myClassList"
+      :default-sort = "{prop: 'downCount', order: 'descending'}"
+    >
+      <el-table-column label="序号" width="70" align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="上传用户" width="150">
+        <template slot-scope="scope">
+          <!-- 用户头像 -->
+          <div>
+            <el-avatar :src="scope.row.avatar" :size="small"></el-avatar>
+          </div>
+          <!-- 用户昵称 -->
+          <div>
+            <el-link type="primary">{{ scope.row.nickname }}</el-link>
+          </div>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="sourceName" label="资源名称">
+      </el-table-column>
+
+      <el-table-column prop="downCount" label="下载数量" sortable>
+      </el-table-column>
+
+      <el-table-column label="文件大小">
+        <template slot-scope="scope"> {{ scope.row.fileSize }}/MB </template>
+      </el-table-column>
+      <el-table-column label="所需积分" sortable>
+        <template slot-scope="scope">
+          {{ scope.row.price}}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="上传日期" sortable width="150">
+        <template slot-scope="scope">
+          {{ scope.row.gmtCreate.substr(0, 10) }}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" width="350" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/dzd/article/info/' + scope.row.id">
+            <el-button type="text" size="mini" icon="el-icon-edit"
+              >暗箱操作</el-button
+            >
+          </router-link>
+          <el-button
+            type="text"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeById(scope.row.id)"
+            >删除</el-button
+          >
+          <el-button
+            type="text"
+            size="mini"
+            icon="el-icon-s-tools"
+            @click="banById(scope.row.id)"
+          >
+            {{ scope.row.isBan == 0 ? "封禁" : "解封" }}
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 分页 -->
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getPublicPageList"
+    />
+  </div>
+</template>
+<script>
+import article from "@/api/dzd/article";
+import source from "@/api/dzd/source";
+
+export default {
+  data() {
+    return {
+      listLoading: false, // 是否显示loading信息
+      total: 0, // 总记录数
+      page: 1, // 页码
+      limit: 10, // 每页记录数
+      searchObj: {}, // 查询条件、
+      key: 0,
+      sourceList: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      //获取文章列表分页
+      this.getPublicPageList();
+    },
+
+    // 条件查询带分页
+    getPublicPageList(page = 1) {
+      this.page = page;
+      this.listLoading = true;
+      source.getPublicPageList(this.page, this.limit, this.searchObj)
+        .then((response) => {
+          this.sourceList = response.data.items;
+          this.total = response.data.total;
+          this.listLoading = false;
+        });
+    },
+
+    banById(id) {
+      this.$confirm("此操作将封禁/解封改资源, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          return source.banById(id);
+        })
+        .then(() => {
+          this.getPublicPageList();
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        })
+        .catch((response) => {
+          // 失败
+          if (response === "cancel") {
+            this.$message({
+              type: "info",
+              message: "已取消更改",
+            });
+          } else {
+            this.$message({
+              type: "error",
+              message: "修改失败",
+            });
+          }
+        });
+    },
+
+    //清空
+    resetData() {
+      this.searchObj = {};
+      this.page = 1;
+      this.getPageList();
+    },
+
+    //删除资源by Id
+    removeById(id, page) {
+      this.$confirm("此操作将永久删除该记录, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          return source.removeById(id);
+        })
+        .then(() => {
+          this.getPublicPageList();
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        })
+        .catch((response) => {
+          // 失败
+          if (response === "cancel") {
+            this.$message({
+              type: "info",
+              message: "已取消删除",
+            });
+          } else {
+            this.$message({
+              type: "error",
+              message: "删除失败",
+            });
+          }
+        });
+    },
+  },
+};
+</script>
