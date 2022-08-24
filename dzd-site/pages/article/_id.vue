1e429fcdf0ee97a96d53168807ebf3ebd11fcc37
dizhongdi <2755063993@qq.com>
2022-08-25 00:00:03 +0800

    文章

---
+++ dzd-site/pages/article/_id.vue
@@ -0,0 +1,123 @@
+<template>
+    <section class="container">
+      <br />
+      <div style="text-align: center;" >
+        <b style="font-size: 40px; margin:0 auto" >弟中弟项目文章详情</b>
+       </div>
+    <el-card class="box-card" style="border-radius: 30px">
+      <div slot="header" class="clearfix">
+        <el-row >
+
+            <el-col :span="1" >
+                <div >
+                  <el-tag type="success">原创</el-tag>
+                </div>
+            </el-col>
+
+            <!-- 昵称 -->
+            <el-col :span="2" :offset="1">
+                <div style="margin: 0 auto">
+                   <div style="font-size:16px; color: #999999;">用户名</div>
+                </div>
+            </el-col>
+            <!-- 发布时间-->
+            <el-col :span="7" :offset="13">
+                <div style="font-size:19px; color: #888888;">
+                     <i class="el-icon-time"  style="margin: 0 10px; font-size: 20px; color: #888888">于2022-08-25 11:22发布</i>
+                </div>
+            </el-col>
+
+        </el-row>
+        <el-row>
+                <!-- 用户头像 -->
+              <el-col :span="1" :offset="2"><div >
+                        <el-avatar></el-avatar>
+                    </div>
+                </el-col>
+            </el-row>
+
+
+            <el-row>
+              <!-- 分类-->
+              <el-col :span="6" :offset="1">
+                  <div>
+                      文章分类：
+                  </div>
+              </el-col>
+
+                <!-- 浏览量-->
+                <el-col :span="3" :offset="15">
+                    <div>
+                        <i class="el-icon-view"  style="margin: 0 10px; font-size: 18px; color:  #777777">200浏览</i>
+                    </div>
+                </el-col>
+                    <!-- 点赞 -->
+                <el-col :span="3" >
+                    <div>
+                        <i class="el-icon-thumb"  style="margin: 0 10px; font-size: 18px; color:  #777777" >300点赞</i>
+                    </div>
+                </el-col>
+                <!-- 评论 -->
+                <el-col :span="3" >
+                    <div>
+                        <i class="el-icon-s-comment"  style="margin: 0 10px; font-size: 18px; color: #777777" >300评论</i>
+                    </div>
+                </el-col>
+
+             </el-row>
+      </div>
+
+字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。字体介绍：
+
+方正新楷体是方正楷体的深度优化版。方正楷体制作年代较早，复刻自铅字楷体字稿，限于当时的技术手段，细节不够完善。方正新楷体的笔法更为讲究，笔画分明利落，字形端正，细节精致，曲线节点的位置都有明确规则。相对原方正楷体，方正新楷体的字面稍大，中宫也稍放松，文本灰度均匀统一，是一款优良的正文排版字体。
+    </el-card>
+
+    </section>
+
+</template>
+
+<script>
+  import article from '@/api/article'
+  export default {
+      data () {
+        return {
+          articleInfo:[], //帖子列表
+        }
+      },
+
+      asyncData({ params, error }) {
+        return {id: params.id}
+
+      },
+      //加载完渲染时
+      created () {
+        //获取文章详情
+        this.getArticleInfo()
+      },
+
+      methods: {
+        //获取文章详情
+        getArticleInfo(){
+          article.getArticleInfo(this.id).then((response) => {
+                this.Articlelist = response.data.data.item
+            });
+        }
+
+       }
+
+    }
+</script>
